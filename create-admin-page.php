1359ee0a43754adaa18fd2491b3611e65629f2f6
spjpierson <spjpierson@gmail.com>
2023-01-10 21:50:34 -0600

    starting the plugin

--- create-admin-page.php
+++ create-admin-page.php
@@ -18,8 +18,54 @@ class CreateAdminPage{
     }
 
     public function display_page_content(){
-        echo "test";
-    }
+        ?>
+        <style>
+            table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 98%;
+            margin-top:1%;
+        
+        }
+  
+        td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+  }
+  
+        tr:nth-child(even) {
+         background-color: #dddddd;
+  }
+       </style> 
+        <?php
 
+        
+        // Get all products
+        $products = wc_get_products( array('limit' => -1) );
+
+        echo '<table>';
+        echo '<tr>';
+        echo '<th>ID</th>';
+        echo '<th>Tumbnail</th>';
+        echo '<th>Product</th>';
+        echo '<th>Type</th>';
+        echo '<th>Price</th>';
+        echo '<tr>';
+        // Loop through each product
+        foreach( $products as $product ) {
+            echo '<tr>';
+            echo '<td>'.$product->get_id().'</td>';
+            echo '<td>';
+            echo '<img height="25px" width="25px" src="'.wp_get_attachment_url( $product->get_image_id() ).'"/>';
+            echo'</td>';
+            echo '<td>'.$product->get_name().'</td>';
+            echo '<td>'.$product->get_type().'</td>';
+            echo '<td>'.floatval($product->get_price()).'</td>';
+            echo '</tr>';
+        }
+
+        echo '<table>';
+    }
 }
 ?>
--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 
 /*
 Plugin Name: Quick Price Edit For WooCommerce
-Description: A plugin to update the price of products in bulks in Woocommerce.
+Description: A plugin to update the price of products quickly.
 Version: 1.0
 Author: Spencer Pierson
 */
-
 
 if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
     die ('You need WooCommerce for this plugin to work.');
